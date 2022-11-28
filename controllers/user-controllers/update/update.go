891761172ef717b44b2ab4c8d6f1edc8c6a6c946
Diego Prestes <diegoprestes.git@hotmail.com>
2022-11-28 00:09:51 -0300

    feat: update user

--- controllers/user-controllers/update/update.go
+++ controllers/user-controllers/update/update.go
@@ -1,7 +1,33 @@
 package updateUser
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"net/http"
+	config "themoviebakery/config"
+	createUser "themoviebakery/controllers/user-controllers/create"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+)
 
 func UpdateUser(ginContext *gin.Context) {
+	mongoNewConnection := config.ConnectMongo()
 
+	var userBody createUser.UserType
+	ginContext.ShouldBindJSON(&userBody)
+
+	userBody.UpdatedAt = time.Now()
+
+	update := bson.D{{"$set", userBody}}
+
+	// res, err := mongoNewConnection.Collection.UpdateOne(context.TODO(), bson.M{"_id": userBody.Id}, {$set: userBody})
+	res, err := mongoNewConnection.Collection.UpdateOne(context.TODO(), bson.M{"_id": userBody.Id}, update)
+	if err != nil {
+		panic(err)
+	}
+
+	defer mongoNewConnection.Disconnect()
+
+	ginContext.IndentedJSON(http.StatusOK, res)
 }
